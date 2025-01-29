613c32868d7928ed60411072a55984ad195affe9
real-Sandip-Das <sd13535@outlook.com>
2025-01-29 14:08:01 +0530

    Tested and debugged switchProblem.bat

--- switchProblem.bat
+++ switchProblem.bat
@@ -8,11 +8,10 @@ if "%~1"=="" (
 setlocal EnableDelayedExpansion
 for /l %%i in (1,1,%1) do (
     if not exist "cp%%i" (
-        xcopy /e /i "cp%%i-1" "cp%%i"
+        xcopy /e /i "cp!%%i-1!" "cp%%i"
     )
 )
 
 ren cp cptemp
 ren "cp%1" cp
 ren cptemp "cp%1"
-
