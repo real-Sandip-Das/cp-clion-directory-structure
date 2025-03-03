b51334f2fab3899bb6aa6786ccbd36f253aa5e42
real-Sandip-Das <sd13535@outlook.com>
2025-03-03 23:24:00 +0530

    New template with shortcuts and debug macros

--- cp/main.cpp
+++ cp/main.cpp
@@ -1,18 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
+// debug macros
+template<typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = enable_if_t<!is_same_v<T_container, string>, typename T_container::value_type>>
+ostream &operator<<(ostream &os, const T_container &v) {
+    os << '{';
+    string sep;
+    for (const T &x: v) os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail>
+void dbg_out(const Head &H, Tail... T) {
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+// code shortening macros
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define RREP(i, b, a) for (int i = b; i >= a; i--)
+#define EACH(x, v) for (auto x: v)
+#define REACH(x, v) for (auto &x: v)
+#define sz(x) ((int) x.size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin, (x).rend()
+typedef long long ll;
+typedef long double ld;
+typedef double d;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+constexpr ll MOD = 1e9 + 7;
+constexpr ll INF = 1e9;
+constexpr ld EPS = 1e-9;
+
+void solve() {
 }
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
     // solve();
 
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) {
         solve();
     }
