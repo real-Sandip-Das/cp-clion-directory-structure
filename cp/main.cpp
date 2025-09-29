eca7bc5386e2805d662d004a617beaa3467af7f2
Sandip Das <sd13535@outlook.com>
2025-09-29 23:28:05 +0530

    Added template<typename T> is PB set and added indexed_multiset

--- cp/main.cpp
+++ cp/main.cpp
@@ -2,7 +2,10 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
 using namespace __gnu_pbds;
-typedef tree<int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+template<typename T>
+using indexed_set = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T>
+using indexed_multiset = tree<T, null_type, less_equal<>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // debug macros
 template<typename A, typename B>
